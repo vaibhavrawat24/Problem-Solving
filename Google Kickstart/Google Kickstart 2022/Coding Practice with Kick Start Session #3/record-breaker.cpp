b2aff0f02076b02d8493c6c662a03fae9a1db25a
Vaibhav <rawatvaibhav42@gmail.com>
2022-08-28 14:18:32 +0530

    Edited the code

--- Google Kickstart/Google Kickstart 2022/Coding Practice with Kick Start Session #3/record-breaker.cpp
+++ Google Kickstart/Google Kickstart 2022/Coding Practice with Kick Start Session #3/record-breaker.cpp
@@ -1,4 +1,27 @@
-//Record Breaker
+//Record Breaker (Coding Practice with Kick Start Session #3 - Kick Start 2022)
+
+/*
+Isyana is given the number of visitors at her local theme park on N consecutive days. The number of visitors on the i-th day is Vi. A day is record breaking if it satisfies both of the following conditions:
+
+Either it is the first day, or the number of visitors on the day is strictly larger than the number of visitors on each of the previous days.
+Either it is the last day, or the number of visitors on the day is strictly larger than the number of visitors on the following day.
+Note that the very first day could be a record breaking day!
+Please help Isyana find out the number of record breaking days.
+
+Sample Input :                                       Sample Output:
+4                                                    
+8                                                    Case #1: 2
+1 2 0 7 2 0 2 0                                      
+6                                                    Case #2: 1
+4 8 15 16 23 42                                      
+9                                                    Case #3: 3
+3 1 4 1 5 9 2 6 5
+6                                                    Case #4: 0
+9 9 9 9 9 9
+*/
+
+//Time Complexity:
+//Space Complexity:
 
 #include <bits/stdc++.h>
 using namespace std;
