7c89852ff73ed496f25e56c8ebdb6dea12419700
Vaibhav <rawatvaibhav42@gmail.com>
2022-08-28 14:12:15 +0530

    Edited the code

--- Google Kickstart/Google Kickstart 2022/Coding Practice with Kick Start Session #3/record-breaker.cpp
+++ Google Kickstart/Google Kickstart 2022/Coding Practice with Kick Start Session #3/record-breaker.cpp
@@ -1,67 +1,35 @@
+//Record Breaker
+
 #include <bits/stdc++.h>
 using namespace std;
 
-int max(vector<int> dup)
-{
-    return *max_element(dup.begin(), dup.end());
-}
-
-bool duplicate(vector<int> dup,int n)
-{
-    if(find(dup.begin(),dup.end(),n)!=dup.end())
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-
-int NumberOfRecordBreakingDays(int n, vector<int> v) 
-{
-    vector<int> dup(n);
-    int count=0;
-    if(n==0) return 0;
-    else if(n==1 && v[0]==0) return 0;
-    else if(n==1 && v[0]!=0) return 1;
-    
-    for(int i=0;i<n;i++)
-    {
-        if(i==0 && v[i]>v[i+1] && duplicate(dup,v[i])==false && v[i]>max(dup))
-        {
-            count+=1;
-            dup.push_back(v[i]);
-        }
-        else if(i==n-1 && v[i]>v[i-1] && duplicate(dup,v[i])==false && v[i]>max(dup))
-        {
-            count+=1;
-            dup.push_back(v[i]);
-        }
-        else if(v[i]>v[i-1] && v[i]>v[i+1] && duplicate(dup,v[i])==false && v[i]>max(dup)) 
-        {
-            count+=1;
-            dup.push_back(v[i]);
-        }
-    }
-    return count;
+int NumberOfRecordBreakingDays(int n, vector<int> v) {
+  int maxV=-1;
+  int count=0;
   
-
-  return 0;
+  for(int i=0;i<n;i++)
+  {
+      if(v[i]>maxV && (i==n-1 || v[i]>v[i+1]))
+      {
+          count++;
+      }
+      maxV=max(maxV,v[i]);
+  }
+  
+  return count;
 }
 
 int main() {
   int t;
   cin >> t;
-  for (int i=1; i<=t; i++) {
+  for (int i = 1; i <= t;i++) {
     int n;
     cin >> n;
     vector<int> v(n);
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++) {
       cin >> v[i];
     }
-    cout << "Case #" <<t<< ": " << NumberOfRecordBreakingDays(n,v) << endl;
+    cout << "Case #" << i << ": " << NumberOfRecordBreakingDays(n,v) << endl;
   }
   return 0;
 }
