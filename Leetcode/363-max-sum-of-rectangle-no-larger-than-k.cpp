2a8e8524067d432aae736a79c0411f0974390c30
Vaibhav <rawatvaibhav42@gmail.com>
2022-08-28 15:52:22 +0530

    Added question 363

---
+++ Leetcode/363-max-sum-of-rectangle-no-larger-than-k.cpp
@@ -0,0 +1,37 @@
+//363. Max Sum of Rectangle No Larger Than K
+//Problem Link: https://leetcode.com/problems/max-sum-of-rectangle-no-larger-than-k/
+
+//Time: O(min(m,n)^2.max(m,n).logmax(m,n))
+//Space: O(max(m,n))
+
+class Solution {
+public:
+    int maxSumSubmatrix(vector<vector<int>>& matrix, int k) {
+        int res=INT_MIN,rows=matrix.size(),cols=matrix[0].size();
+        for(int i=0;i<cols;i++)
+        {
+            vector<int> sum(rows);
+            for(int j=i;j<cols;j++)
+            {
+                for(int k=0;k<rows;k++)
+                {
+                    sum[k]+=matrix[k][j];
+                }
+                set<int> s={0};
+                int run_sum=0;
+                for(auto sum:sum)
+                {
+                    run_sum+=sum;
+                    auto it=s.lower_bound(run_sum-k);
+                    if(it != end(s))
+                    {
+                        res=max(res,run_sum-*it);
+                    }
+                    s.insert(run_sum);
+                }
+            }
+        }
+        return res;
+        
+    }
+};
