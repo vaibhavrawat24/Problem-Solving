ae3f3301b1c23ea1304914ff7bc81a891bfd1986
Vaibhav <rawatvaibhav42@gmail.com>
2022-08-29 16:38:45 +0530

    Added Question 1526

---
+++ Leetcode/1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp
@@ -0,0 +1,22 @@
+//1526. Minimum Number of Increments on Subarrays to Form a Target Array
+//Problem Link: https://leetcode.com/problems/minimum-number-of-increments-on-subarrays-to-form-a-target-array/
+
+//Time Complexity:
+//Space Complexity:
+
+class Solution {
+public:
+    int minNumberOperations(vector<int>& target) {
+        
+         int initial = target[0];
+            for (int i = 1; i < target.size(); i++)
+            {
+                if (target[i] > target[i - 1])
+                {
+                    initial += target[i] - target[i - 1];
+                }
+            }
+            return initial;
+        
+    }
+};
