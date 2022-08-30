c047ff6580d652883d5bb7697c494f20f4773e5a
Vaibhav <rawatvaibhav42@gmail.com>
2022-08-30 22:15:12 +0530

    Initial commit

---
+++ Leetcode/628-maximum-product-of-three-numbers.cpp
@@ -0,0 +1,17 @@
+//628. Maximum Product of Three Numbers
+//Problem link: 
+
+//Time Complexity: O(n log n)
+//Space Complexity: O(1)
+
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        return max(nums[n - 1] * nums[0] * nums[1],
+               nums[n - 1] * nums[n - 2] * nums[n - 3]);
+        
+    }
+};
