6e61bef9cb7be81d0e16c4bd6837129eeb67abb8
Vaibhav <rawatvaibhav42@gmail.com>
2022-08-30 21:23:45 +0530

    Edited the code

--- Google Kickstart/Google Kickstart 2022/Coding Practice with Kick Start Session #3/wiggle-walk.cpp
+++ Google Kickstart/Google Kickstart 2022/Coding Practice with Kick Start Session #3/wiggle-walk.cpp
@@ -1,11 +1,13 @@
 //Wiggle Walk (Coding Practice with Kick Start Session #3 - Kick Start 2022)
 
 /*
-Banny has just bought a new programmable robot. Eager to test his coding skills, he has placed the robot in a grid of squares with R rows (numbered 1 to R from north to south) and C columns (numbered 1 to C from west to east). The square in row r and column c is denoted (r,c).
-
-Initially the robot starts in the square (SR, SC). Banny will give the robot N instructions. Each instruction is one of N, S, E, or W, instructing the robot to move one square north, south, east, or west respectively.
-
-If the robot moves into a square that it has been in before, the robot will continue moving in the same direction until it reaches a square that it has not been in before. Banny will never give the robot an instruction that will cause it to move out of the grid.
+Banny has just bought a new programmable robot. Eager to test his coding skills, he has placed the robot in 
+a grid of squares with R rows (numbered 1 to R from north to south) and C columns (numbered 1 to C from west to east). 
+The square in row r and column c is denoted (r,c).Initially the robot starts in the square (SR, SC).
+Banny will give the robot N instructions. Each instruction is one of N, S, E, or W, instructing the robot to 
+move one square north, south, east, or west respectively.If the robot moves into a square that it has been in before,
+the robot will continue moving in the same direction until it reaches a square that it has not been in before. Banny 
+will never give the robot an instruction that will cause it to move out of the grid.
 
 Can you help Banny determine which square the robot will finish in, after following the N instructions?
 
