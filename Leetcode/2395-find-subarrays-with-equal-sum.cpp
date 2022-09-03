23a998821c6f561bcb741cbfab963ad98ac96d12
Vaibhav <rawatvaibhav42@gmail.com>
2022-09-03 21:48:57 +0530

    Initial Commit

---
+++ Leetcode/2395-find-subarrays-with-equal-sum.cpp
@@ -0,0 +1,28 @@
+//2395. Find Subarrays With Equal Sum
+//Problem Link: https://leetcode.com/contest/biweekly-contest-86/problems/find-subarrays-with-equal-sum/
+
+//Time Complexity:
+//Space Complexity:
+
+class Solution {
+public:
+    bool findSubarrays(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> arr(n-1);
+        
+         for(int i=1;i<n;i++)
+         {
+            arr[i-1] = nums[i]+nums[i-1];
+         }
+        
+         for(int i=0;i<n-1;i++)
+         {
+            for(int j=i+1;j<n-1;j++)
+            {
+                if(arr[i] == arr[j])return true;
+            }
+         }
+         return false;
+        
+    }
+};
