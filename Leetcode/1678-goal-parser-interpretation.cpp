9b3717fe8c86da6bfc2fcb83367c98aa02ba5e3a
Vaibhav <rawatvaibhav42@gmail.com>
2022-09-08 21:57:18 +0530

    Initial commit

---
+++ Leetcode/1678-goal-parser-interpretation.cpp
@@ -0,0 +1,21 @@
+//1678. Goal Parser Interpretation
+//Problem Link: https://leetcode.com/problems/goal-parser-interpretation/
+
+//Time Complexity:
+//Space Complexity:
+
+class Solution {
+public:
+    string interpret(string command) {
+        string ans;
+        for(int i=0;i<command.size();i++)
+        {
+            char c=command[i];
+            if(c=='G') ans+="G";
+            else if(c=='(' && command[i+1]==')') ans+="o";
+            else if(c=='(' && command[i+1]=='a' && command[i+2]=='l' && command[i+3]==')') ans+="al";
+        }
+        return ans;
+        
+    }
+};
