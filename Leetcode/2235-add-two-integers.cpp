8c2ef434e931f778a56981684d71c0cb154fd1ce
Vaibhav <rawatvaibhav42@gmail.com>
2022-09-18 20:41:35 +0530

    added ques 2235

---
+++ Leetcode/2235-add-two-integers.cpp
@@ -0,0 +1,13 @@
+//2235. Add Two Integers
+//Problem Link: https://leetcode.com/problems/add-two-integers/
+
+//Time Complexity: O(1)
+//Space Complexity: O(1)
+
+class Solution {
+public:
+    int sum(int num1, int num2) {
+        return num1+num2;
+        
+    }
+};
