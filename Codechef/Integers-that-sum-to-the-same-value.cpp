a3db1e185c1ab1451e3e343df612cef907f00878
Vaibhav <rawatvaibhav42@gmail.com>
2022-09-22 23:17:44 +0530

    added integersumval

---
+++ Codechef/Integers-that-sum-to-the-same-value.cpp
@@ -0,0 +1,18 @@
+//Integers that sum to the same value
+//Problem Link: https://www.codechef.com/START57D/problems/SUMNEQ
+
+//Time Complexity:
+//Space Complexity:
+
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+#define pb push_back
+
+int main(){
+    int n;
+    cin>>n;
+    cout<<(n-1)<<endl;
+ 
+    return 0;
+}
