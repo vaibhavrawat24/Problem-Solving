69bc5fb4a65c087058da6ea55b9da48c96bc145c
Vaibhav <rawatvaibhav42@gmail.com>
2022-09-22 23:18:20 +0530

    added alicenmarks

---
+++ Codechef/Alice-and-marks.cpp
@@ -0,0 +1,18 @@
+//Alice and Marks
+//Problem Link: https://www.codechef.com/START57D/problems/MARKSTW
+
+//Time Complexity:
+//Space Complexity:
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x,y;
+	cin>>x>>y;
+	
+	if(x>=y*2) cout<<"YES"<<endl;
+	else cout<<"NO"<<endl;
+	
+	return 0;
+}
