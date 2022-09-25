403db333e16c433448f7b529987914aa1b11454b
Vaibhav <rawatvaibhav42@gmail.com>
2022-09-25 07:43:22 +0530

    added ques 11

---
+++ Geeks For Geeks/11-sort-an-array-of-0s-1s-and-2s.cpp
@@ -0,0 +1,15 @@
+//Sort an array of 0s, 1s and 2s
+//Problem Link: https://practice.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s4231/1?page=1&category[]=Arrays&curated[]=1&sortBy=accuracy
+
+//Time Complexity: O(n log n)
+//Space Complexity: O(1)
+
+class Solution
+{
+    public:
+    void sort012(int a[], int n)
+    {
+        sort(a,a+n);
+    }
+    
+};
