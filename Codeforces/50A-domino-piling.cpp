bd49b224177826dd2efe60d059e564e7c18b5381
Vaibhav <rawatvaibhav42@gmail.com>
2022-09-26 12:46:04 +0530

    added ques 50A

---
+++ Codeforces/50A-domino-piling.cpp
@@ -0,0 +1,18 @@
+//50A. Domino piling
+//Problem Link: https://codeforces.com/problemset/problem/50/A
+
+//Time Complexity:
+//Space Complexity:
+
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+#define pb push_back
+
+int main(){
+    int m,n;
+    cin>>m>>n;
+    cout<<m*n/2<<endl;
+ 
+    return 0;
+}
