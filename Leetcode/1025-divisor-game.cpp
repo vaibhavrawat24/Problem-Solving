3823c7a771ff1c2050e86c4a74118d74966718dc
Vaibhav <rawatvaibhav42@gmail.com>
2022-09-27 19:44:22 +0530

    added ques 1025

---
+++ Leetcode/1025-divisor-game.cpp
@@ -0,0 +1,14 @@
+//1025. Divisor Game
+//Problem Link: https://leetcode.com/problems/divisor-game/
+
+//Time Complexity:
+//Space Complexity:
+
+class Solution {
+public:
+    bool divisorGame(int n) {
+        if(n%2==0) return true;
+        else return false;
+        
+    }
+};
