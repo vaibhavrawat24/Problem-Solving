233c99e839181bc949f43edde7be01d3a1a95f00
Vaibhav <rawatvaibhav42@gmail.com>
2022-09-30 17:52:14 +0530

    added ques 1739A

---
+++ Codeforces/1739A-immobile-knight.cpp
@@ -0,0 +1,21 @@
+// KEEP MOVING FORWARD !
+
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+#define pb push_back
+
+int main()
+{
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        int r,c;
+        cin>>r>>c;
+
+        cout<<r/2+1<<" "<<c/2+1<<endl;
+    }
+    return 0;
+}
