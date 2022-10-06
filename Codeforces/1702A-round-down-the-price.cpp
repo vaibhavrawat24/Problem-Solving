ed44ef58b72e73a19315861b15c51565c0de82e3
Vaibhav <rawatvaibhav42@gmail.com>
2022-10-06 21:20:24 +0530

    added ques 1702A

---
+++ Codeforces/1702A-round-down-the-price.cpp
@@ -0,0 +1,33 @@
+//1702A. Round Down the Price
+//Problem Link:
+
+//Time Complexity:
+//Space Complexity:
+
+// KEEP MOVING FORWARD !
+
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+#define pb push_back
+
+int main(){
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        ll n;
+    	cin>>n;
+    	ll x=1;
+ 
+    	while(x*10<=n)
+    	{
+    		x*=10;
+    	}
+ 
+    	cout<<n-x<<endl;
+    }
+ 
+    return 0;
+}
