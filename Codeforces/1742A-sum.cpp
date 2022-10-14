d6547bb2c0d15745248aa4443d7494b536a25c70
Vaibhav <rawatvaibhav42@gmail.com>
2022-10-14 21:24:03 +0530

    added ques 1742A

---
+++ Codeforces/1742A-sum.cpp
@@ -0,0 +1,21 @@
+// KEEP MOVING FORWARD !
+
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+#define pb push_back
+
+int main()
+{
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        int x,y,z;
+        cin>>x>>y>>z;
+
+        if(x+y==z || y+z==x || z+x==y) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+}
