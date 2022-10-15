fdf60576babf15aa9c8ed57b2c118efbdf0f3a69
Vaibhav <rawatvaibhav42@gmail.com>
2022-10-15 10:15:37 +0530

    added ques 472A

---
+++ Codeforces/472A-design-tutorial-learn-from-math.cpp
@@ -0,0 +1,22 @@
+//472A. Design Tutorial: Learn from Math
+//Problem Link: https://codeforces.com/problemset/problem/472/A
+
+//Time Complexity:
+//Space Complexity:
+
+// KEEP MOVING FORWARD !
+
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+#define pb push_back
+
+int main(){
+    int n;
+    cin>>n;
+
+    if(n%2==0) cout<<n-4<<" 4";
+    else cout<<n-9<<" 9";
+ 
+    return 0;
+}
