a51e9b19a18502160d3439bbae2edb3799274c36
Vaibhav <rawatvaibhav42@gmail.com>
2022-10-20 16:58:47 +0530

    added ques

---
+++ Codechef/reach-on-time.cpp
@@ -0,0 +1,28 @@
+//Reach on Time
+//Problem Link: https://www.codechef.com/START61D/problems/TIMELY
+
+//Time complexity:
+//Space Complexity:
+
+// KEEP MOVING FORWARD !
+
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+#define pb push_back
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--)
+    {
+        int x;
+        cin>>x;
+        
+        if(x<30) cout<<"NO"<<endl;
+        else cout<<"YES"<<endl;
+    }
+ 
+    return 0;
+}
