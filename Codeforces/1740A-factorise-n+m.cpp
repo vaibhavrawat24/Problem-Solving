bbc75715963c3dc7656b7fa42dea692645abca4c
Vaibhav <rawatvaibhav42@gmail.com>
2022-10-30 17:30:50 +0530

    added ques 1740A

---
+++ Codeforces/1740A-factorise-n+m.cpp
@@ -0,0 +1,27 @@
+//1740A Factorise n+m
+//Problem Link: https://codeforces.com/problemset/problem/1740/A
+
+//Time Complexity:
+//Space Complexity:
+
+// KEEP MOVING FORWARD !
+
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+#define pb push_back
+
+int main(){
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        int n;
+        cin>>n;
+
+        cout<<n<<endl;
+    }
+ 
+    return 0;
+}
