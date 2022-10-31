5b7d09778baa459733223084ebacaf453c73f1e3
Vaibhav <rawatvaibhav42@gmail.com>
2022-10-31 10:16:35 +0530

    added ques 630A

---
+++ Codeforces/630A-again-twenty-five!.cpp
@@ -0,0 +1,21 @@
+//630A. Again Twenty Five!
+//Problem Link: https://codeforces.com/problemset/problem/630/A
+
+//Time Complexity:
+//Space Complexity:
+
+// KEEP MOVING FORWARD !
+
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+#define pb push_back
+
+int main(){
+    ll n;
+    cin>>n;
+    
+    cout<<25<<endl;
+ 
+    return 0;
+}
