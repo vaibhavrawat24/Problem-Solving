1e891c58622845aba85244314064a4be07c3266c
Vaibhav <rawatvaibhav42@gmail.com>
2022-11-01 11:22:03 +0530

    added ques 1370A

---
+++ Codeforces/1370A-maximum-GCD.cpp
@@ -0,0 +1,27 @@
+//1370A. Maximum GCD
+//Problem Link: https://codeforces.com/problemset/problem/1370/A
+
+//Time Complexity:
+//Space Complexity:
+
+// KEEP MOVING FORWARD !
+
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+#define pb push_back
+
+int main(){
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        int n;
+        cin>>n;
+
+        cout<<n/2<<endl;
+    }
+ 
+    return 0;
+}
