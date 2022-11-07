19ffb2869a0e6af34f02342b9011244643b21bab
Vaibhav <rawatvaibhav42@gmail.com>
2022-11-07 15:34:06 +0530

    added ques 122A

---
+++ Codeforces/122A-lucky-division.cpp
@@ -0,0 +1,39 @@
+//122A. Lucky Division
+//Problem Link: https://codeforces.com/problemset/problem/122/A
+
+//Time Complexity:
+//Space Complexity:
+
+// KEEP MOVING FORWARD !
+
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+#define pb push_back
+template<class T> using mxpq = priority_queue<T>;
+template<class T> using mnpq = priority_queue<T, vector<T>, greater<T>>;
+
+bool prime(int n)
+{
+    vector<int> prime(n+1,true);
+    prime[1]=false;
+    for(int i=2;i<=n;i++)
+    {
+        if(prime[i]==true)
+        {
+            for(int j=2*i;j<=n;j+=i) prime[j]=false;
+        }
+    }
+    if(prime[n]==true) return true;
+    else return false;
+}
+ 
+int main(){
+    int n;
+    cin>>n;
+
+   if(n%4==0 || n%7==0 || n%44==0|| n%47==0 || n%74==0 ||n%77==0 || n%444==0 || n%447==0 || n%474==0 || n%744==0 || n%477==0 || n%747==0 || n%774==0 || n%777==0 ) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+ 
+    return 0;
+}
