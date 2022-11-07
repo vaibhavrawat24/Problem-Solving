a533a8127eb612f06831e36bc1cd298ebdb3cf2d
Vaibhav <rawatvaibhav42@gmail.com>
2022-11-07 17:07:42 +0530

    added ques 119

---
+++ Leetcode/119-pascal's-triangle-II.cpp
@@ -0,0 +1,25 @@
+//119. Pascal's Triangle II
+//Problem Link: https://leetcode.com/problems/pascals-triangle-ii/
+
+//Time Complexity: O(n2)
+//Space Complexity: O(n)
+
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<vector<int>> dp(rowIndex+1);
+        dp[0].push_back(1);
+        
+        for(int i=1;i<=rowIndex;i++)
+        {
+            dp[i].push_back(1);
+            for(int j=1;j<i;j++)
+            {
+                dp[i].push_back(dp[i-1][j-1]+dp[i-1][j]);
+            }
+            dp[i].push_back(1);
+        }
+        
+        return dp[rowIndex];
+    }
+};
