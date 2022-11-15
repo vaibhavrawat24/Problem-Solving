8c2f6bc3e6514e2c05bab520465aa953f50c0fd1
Vaibhav <rawatvaibhav42@gmail.com>
2022-11-15 09:35:48 +0530

    added ques 222

--- Leetcode/222-count-complete-tree-nodes.cpp
+++ Leetcode/222-count-complete-tree-nodes.cpp
@@ -14,3 +14,36 @@ public:
         return 1+countNodes(root->left)+countNodes(root->right);
     }
 };
+
+//Approach 2: Find the complete binary tree at earlier stage
+
+//Time Complexity: O(log^2 n)
+//Space Complexity: O(h)
+
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if(root==NULL) return 0;
+        
+        TreeNode * l=root;
+        TreeNode * r=root;
+        int l_lt=0,r_rt=0;
+        
+        while(l)
+        {
+            l=l->left;
+            l_lt+=1;
+        }
+        
+        while(r)
+        {
+            r=r->right;
+            r_rt+=1;
+        }
+        
+        if(l_lt==r_rt) return (pow(2,l_lt)-1);
+        
+        return (1+countNodes(root->left)+countNodes(root->right));
+        
+    }
+};
