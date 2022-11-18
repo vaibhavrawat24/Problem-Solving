dd590997c12e6890861a574ffec4491c0c6f59cf
Vaibhav <rawatvaibhav42@gmail.com>
2022-11-18 10:44:17 +0530

    added ques 263

---
+++ Leetcode/263-ugly-number.cpp
@@ -0,0 +1,22 @@
+//263. Ugly Number
+//Problem Link: https://leetcode.com/problems/ugly-number/
+
+//Time Complexity: O(n)
+//Space Complexity: O(1)
+
+class Solution {
+public:
+    bool isUgly(int n) {
+        if(n<0) return false;
+        
+        while(n>0)
+        {
+            if(n%2==0) n=n/2;
+            else if(n%3==0) n=n/3;
+            else if(n%5==0) n=n/5;
+            else break;
+        }
+        
+        return (n==1);
+    }
+};
