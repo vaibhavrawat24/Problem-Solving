332f327f4075f3ea240f9ca7919367712e0f0f5b
Vaibhav <rawatvaibhav42@gmail.com>
2022-11-26 12:51:57 +0530

    added tabulation method

--- Leetcode/198-house-robber.cpp
+++ Leetcode/198-house-robber.cpp
@@ -32,6 +32,29 @@ public:
 
 //Approach 2: Tabulation (Bottom-up)
 
-//Time Complexity:
-//Space Complexity:
+//Time Complexity: O(n)
+//Space Complexity: O(n)
 
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        int prev=nums[0];
+        int prev2=0;
+        
+        for(int i=1;i<n;i++)
+        {
+            int take=nums[i];
+            if(i>1) take+=prev2;
+            
+            int not_take=0+prev;
+            
+            int curi=max(take,not_take);
+            
+            prev2=prev;
+            prev=curi;
+        }
+        
+        return prev;
+    }
+};
