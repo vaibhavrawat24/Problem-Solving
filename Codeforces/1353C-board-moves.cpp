07a158eac92a7c7f9539dc1c7a6a6c60dbde6e96
Vaibhav <rawatvaibhav42@gmail.com>
2022-11-29 08:42:34 +0530

    added ques 1353C

---
+++ Codeforces/1353C-board-moves.cpp
@@ -0,0 +1,50 @@
+//1353C. Board Moves
+//Problem Link: https://codeforces.com/problemset/problem/1353/C
+
+//time complexity:
+//Space complexity:
+
+// KEEP MOVING FORWARD !
+
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+#define pb push_back
+template<class T> using mxpq = priority_queue<T>;
+template<class T> using mnpq = priority_queue<T, vector<T>, greater<T>>;
+
+bool prime(int n)
+{
+    vector<int> prime(n+1,true);
+    prime[1]=false;
+    for(int i=2;i<=n;i++)
+    {
+        if(prime[i]==true)
+        {
+            for(int j=2*i;j<=n;j+=i) prime[j]=false;
+        }
+    }
+    if(prime[n]==true) return true;
+    else return false;
+}
+ 
+int main(){
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        ll n,ans=0,v=0;
+        cin>>n;
+
+        for(int i=0;i<=(n/2);i++)
+        {
+            ans+=i*v;
+            v+=8;
+        }
+
+        cout<<ans<<endl;
+    }
+ 
+    return 0;
+}
