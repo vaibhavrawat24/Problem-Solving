52e71a96e816d038a73dfd15119b4806966b1aaa
Vaibhav <rawatvaibhav42@gmail.com>
2023-01-09 12:03:13 +0530

    added ques 405A

---
+++ Codeforces/405A-gravity-flip.cpp
@@ -0,0 +1,46 @@
+//405A. Gravity Flip
+//Problem Link: https://codeforces.com/problemset/problem/405/A
+
+//time complexity: O(n log n)
+//Space Complexity: O(n)
+
+// KEEP MOVING FORWARD !
+
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+#define pb push_back
+template<class T> using mxpq = priority_queue<T>;
+template<class T> using mnpq = priority_queue<T, vector<T>, greater<T>>;
+
+bool prime(int n)
+{
+    vector<int> prime(n+1,true);
+    prime[1]=false;
+    for(int i=2;i<=n;i++)
+    {
+        if(prime[i]==true)
+        {
+            for(int j=2*i;j<=n;j+=i) prime[j]=false;
+        }
+    }
+    if(prime[n]==true) return true;
+    else return false;
+}
+ 
+int main(){
+    int n;
+    cin>>n;
+
+    vector<int> a(n);
+
+    for(int i=0;i<n;i++) cin>>a[i];
+
+    sort(a.begin(),a.end());
+
+    for(auto it:a) cout<<it<<" ";
+
+    cout<<endl;
+ 
+    return 0;
+}
