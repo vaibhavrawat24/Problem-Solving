32f9e22859d80173415f1d485978ae69bc5f4e77
Vaibhav <rawatvaibhav42@gmail.com>
2023-01-26 11:56:36 +0530

    edited code

--- Geeks For Geeks/37-0-1-knapsack-problem.cpp
+++ Geeks For Geeks/37-0-1-knapsack-problem.cpp
@@ -1,10 +1,10 @@
 //37 0 - 1 Knapsack Problem
 //Problem Link: https://practice.geeksforgeeks.org/problems/0-1-knapsack-problem0945/1
 
-//Memoization
+//Memoization (Top down)
 
 //Time Complexity: O(n*w)
-//Space Complexity: O(n*w)
+//Space Complexity: O(n*w) + O(n) (extra stack space for recursion)
 
 class Solution
 {
@@ -32,4 +32,41 @@ class Solution
         
         return solve(W,wt,val,n,dp);
     }
-}
+};
+
+//Tabulation (Bottom Up)
+
+//Time Complexity: O(n*w)
+//Space Complexity: O(n*w)
+
+int knapSack(int W, int wt[], int val[], int n) 
+    { 
+        vector<vector<int>> dp(n+1,vector<int>(W+1,-1));
+        
+        for(int i=0;i<n+1;i++)
+        {
+            for(int j=0;j<W+1;j++)
+            {
+                if(i==0 || j==0) dp[i][j]=0;
+            }
+        }
+        
+        for(int i=1;i<n+1;i++)
+        {
+            for(int j=1;j<W+1;j++)
+            {
+                
+                if(wt[i-1]<=j)
+                {
+                    dp[i][j]=max(val[i-1]+dp[i-1][j-wt[i-1]],dp[i-1][j]);
+                }
+                else
+                {
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        
+        return dp[n][W];
+    }
+    
