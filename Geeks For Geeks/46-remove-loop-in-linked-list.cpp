6a6674f9ad2645e53be21d29fab35579cc7a028d
Vaibhav <rawatvaibhav42@gmail.com>
2023-02-07 14:32:12 +0530

    added ques 46

---
+++ Geeks For Geeks/46-remove-loop-in-linked-list.cpp
@@ -0,0 +1,42 @@
+//46. Remove loop in Linked List
+//Problem Link: https://practice.geeksforgeeks.org/problems/remove-loop-in-linked-list/1
+
+//Time Complexity:
+//Space Complexity:
+
+class Solution
+{
+    public:
+    //Function to remove a loop in the linked list.
+    void removeLoop(Node* head)
+    {
+        Node* slow=head,*fast=head;
+        
+        while(fast!=NULL && fast->next!=NULL)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+            
+            if(slow==fast) 
+            {
+                break;
+            }
+        }
+        
+        if(fast==NULL || fast->next==NULL) return;
+        
+        fast=head;
+        
+        while(slow!=fast)
+        {
+            slow=slow->next;
+            fast=fast->next;
+        }
+        
+        while(slow->next!=fast) slow=slow->next;
+        
+        slow->next=NULL;
+        
+        return;
+    }
+};
