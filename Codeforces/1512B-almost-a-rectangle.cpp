9f7a285203eecf1bdfa9869ae6bccb80af780518
Vaibhav <rawatvaibhav42@gmail.com>
2023-02-24 21:14:42 +0530

    added ques

---
+++ Codeforces/1512B-almost-a-rectangle.cpp
@@ -0,0 +1,27 @@
+// KEEP MOVING FORWARD !
+
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+#define pb push_back
+template<class T> using mxpq = priority_queue<T>;
+template<class T> using mnpq = priority_queue<T, vector<T>, greater<T>>;
+
+bool prime(int n)
+{
+    vector<int> prime(n+1,true);
+    prime[1]=false;
+    for(int i=2;i<=n;i++)
+    {
+        if(prime[i]==true)
+        {
+            for(int j=2*i;j<=n;j+=i) prime[j]=false;
+        }
+    }
+    if(prime[n]==true) return true;
+    else return false;
+}
+
+int main(){
+
+}
