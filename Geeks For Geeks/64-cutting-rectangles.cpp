66e81dd534d80e5b5953ac3ff5fd07ebe2eb954c
Vaibhav <rawatvaibhav42@gmail.com>
2023-03-03 12:35:01 +0530

    added ques 64

---
+++ Geeks For Geeks/64-cutting-rectangles.cpp
@@ -0,0 +1,15 @@
+//64.Cutting Rectangles
+//Problem Link: https://practice.geeksforgeeks.org/problems/a7a4da81b20f4a05bbd93f5786fcf7478298f4f5/1
+
+//Time complexity: O(1)
+//Space complexity: O(1)
+
+class Solution{
+public:
+    vector<long long int> minimumSquares(long long int L, long long int B)
+    {
+        long long int k=__gcd(L,B);
+        long long int n=L/k*B/k;
+        return {n,k};
+    }
+};
