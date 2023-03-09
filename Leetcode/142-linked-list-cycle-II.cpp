e52c6dbba4c7003848f06950d1c292198e94c965
Vaibhav <rawatvaibhav42@gmail.com>
2023-03-09 22:23:49 +0530

    added ques 142

---
+++ Leetcode/142-linked-list-cycle-II.cpp
@@ -0,0 +1,28 @@
+//142. Linked List Cycle II
+//Problem Link: https://leetcode.com/problems/linked-list-cycle-ii/description/
+
+//Time Complexity:
+//Space complexity:
+
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if (!head || !head->next) return nullptr; 
+        
+        ListNode *tortoise = head, *hare = head;
+        while (hare && hare->next) {
+            tortoise = tortoise->next;
+            hare = hare->next->next;
+            if (tortoise == hare) { 
+                hare = head;
+                while (tortoise != hare) 
+                {
+                    tortoise = tortoise->next;
+                    hare = hare->next;
+                }
+                return hare; 
+            }
+        }
+        return nullptr; 
+    }
+};
