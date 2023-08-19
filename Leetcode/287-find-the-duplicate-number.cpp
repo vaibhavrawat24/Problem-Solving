e3ab1e44eaabd51b7bc4b54bd4c3006260a5a8eb
Vaibhav <rawatvaibhav42@gmail.com>
2023-08-19 15:25:20 +0530

    added ques 287

---
+++ Leetcode/287-find-the-duplicate-number.cpp
@@ -0,0 +1,31 @@
+// 287. Find the Duplicate Number
+// Problem Link: https://leetcode.com/problems/find-the-duplicate-number/description/
+
+// Time Complexity:
+// Space Complexity:
+
+class Solution
+{
+public:
+    int findDuplicate(vector<int> &nums)
+    {
+        int hare = nums[0];
+        int tortoise = nums[0];
+
+        do
+        {
+            tortoise = nums[tortoise];
+            hare = nums[nums[hare]];
+        } while (tortoise != hare);
+
+        tortoise = nums[0];
+
+        while (tortoise != hare)
+        {
+            tortoise = nums[tortoise];
+            hare = nums[hare];
+        }
+
+        return tortoise;
+    }
+};
