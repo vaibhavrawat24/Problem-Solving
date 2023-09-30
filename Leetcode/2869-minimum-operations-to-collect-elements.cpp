0a1bf657604221e2b914bc7a54f0bc9ba294ddb3
Vaibhav <rawatvaibhav42@gmail.com>
2023-09-30 22:16:59 +0530

    added ques 2869

---
+++ Leetcode/2869-minimum-operations-to-collect-elements.cpp
@@ -0,0 +1,28 @@
+// 2869. Minimum operations to collect elements
+// Problem Link: https://leetcode.com/contest/biweekly-contest-114/problems/minimum-operations-to-collect-elements/
+
+// Time Complexity:
+// Space Complexity:
+
+class Solution
+{
+public:
+    int minOperations(vector<int> &nums, int k)
+    {
+        set<int> val;
+        int move = 0;
+
+        for (int i = nums.size() - 1; i >= 0; i--)
+        {
+            move++;
+
+            if (nums[i] <= k)
+                val.insert(nums[i]);
+
+            if (val.size() == k)
+                return move;
+        }
+
+        return 0;
+    }
+};
